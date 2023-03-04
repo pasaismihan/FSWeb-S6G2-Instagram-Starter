17b00e4952ba27368739867163783d46b9adfaad
pasa ismihan <pasa_13142@hotmail.com>
2023-03-04 22:52:35 +0300

    instagram guncellendi

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@
 */
 
 // State hook u import edin
-import React from 'react';
-
+import React from "react";
+import { useState } from "react";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
+import sahteVeri from "./sahte-veri";
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+import "./App.css";
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [aramaKriteri, setAramaKriteri] = useState(null);
+
+  const aramaYap = (e) => {
+    setAramaKriteri(e);
+    setGonderiler(sahteVeri.filter((element) => element.username.includes(e)));
+  };
+
+  const gonderiyiBegen = (gonderiID) => {
+    gonderiler.find((e) => e.id == gonderiID).likes += 1;
+    setGonderiler([...sahteVeri]);
+
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -31,7 +44,9 @@ const App = () => {
   };
 
   return (
-    <div className='App'>
+    <div className="App">
+      <AramaÇubuğu arama={aramaKriteri} aramaYap={aramaYap} />
+      <Gönderiler gonderiler={gonderiler} gonderiyiBegen={gonderiyiBegen} />
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
--- src/bileşenler/Gönderiler/Gönderiler.js
+++ src/bileşenler/Gönderiler/Gönderiler.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Gönderi from './Gönderi';
-import './Gönderiler.css';
+import React from "react";
+import Gönderi from "./Gönderi";
+import "./Gönderiler.css";
 
 const Gönderiler = (props) => {
   // 🔥 Gönderiler'in ebeveyninin doğru değişkenleri doğru şekilde ilettiğine emin olun!
   const { gonderiyiBegen, gonderiler } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
+      {gonderiler.map((element) => (
+        <Gönderi
+          gönderi={element}
+          gonderiyiBegen={gonderiyiBegen}
+          key={element.id}
+        />
+      ))}
       {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
     </div>
   );
